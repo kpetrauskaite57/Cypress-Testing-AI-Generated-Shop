3edc4e05b556316f3c6410102c600baa17a7fa60
kpetrauskaite57 <kpetrauskaite57@gmail.com>
2024-08-28 11:33:56 +0300

    App.js

--- my-eshop/src/App.js
+++ my-eshop/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Cart from './components/Cart';
+import './style.css'; // Assuming you have a global style file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My E-Shop</h1>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
   );
 }
 
